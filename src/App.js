993f40db5235d23bba53271fc524cb25c1adca05
Jennas-Lee <leezeunjun@gmail.com>
2021-01-25 21:42:34 +0900

    Create router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink as RRNavLink } from 'react-router-dom';
 import { Container, Form, Row, Col, Button, Input, Label, Nav, NavItem, NavLink } from 'reactstrap';
 
 function Login(props) {
@@ -27,32 +27,68 @@ function Login(props) {
 
 function Main(props) {
     return (
-        <Nav className="flex-column main-nav">
-            <NavItem>
-                <NavLink className="text-center">
-                    <img src="./icons/chat-left.svg" alt=""/>
-                    <p>메신저</p>
-                </NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink className="text-center">
-                    <img src="./icons/award.svg" alt=""/>
-                    <p>상점</p>
-                </NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink className="text-center">
-                    <img src="./icons/calendar-date.svg" alt=""/>
-                    <p>일정</p>
-                </NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink className="text-center">
-                    <img src="./icons/clock.svg" alt=""/>
-                    <p>시간표</p>
-                </NavLink>
-            </NavItem>
-        </Nav>
+        <div className="main-nav">
+            <Nav className="flex-column">
+                <BrowserRouter>
+                    <NavItem>
+                        <NavLink className="text-center text-body" to="/messenger" tag={RRNavLink}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-chat-left" viewBox="0 0 16 16">
+                                <path
+                                    d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                            </svg>
+                            <p className="mt-1">메신저</p>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className="text-center text-body" to="/point" tag={RRNavLink}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-award" viewBox="0 0 16 16">
+                                <path
+                                    d="M9.669.864L8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193l.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z"/>
+                                <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+                            </svg>
+                            <p className="mt-1">상벌점</p>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className="text-center text-body" to="/calendar" tag={RRNavLink}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-calendar-date" viewBox="0 0 16 16">
+                                <path
+                                    d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z"/>
+                                <path
+                                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+                            </svg>
+                            <p className="mt-1">일정</p>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className="text-center text-body" to="/timetable" tag={RRNavLink}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-clock" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+                            </svg>
+                            <p className="mt-1">시간표</p>
+                        </NavLink>
+                    </NavItem>
+                    <NavItem className="setting-btn">
+                        <NavLink className="text-center text-body" to="/settings" tag={RRNavLink}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-gear" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+                                <path
+                                    d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
+                            </svg>
+                            <p className="mt-1">설정</p>
+                        </NavLink>
+                    </NavItem>
+                </BrowserRouter>
+            </Nav>
+        </div>
     );
 }
 
@@ -60,9 +96,12 @@ function App() {
     return (
         <main>
             <Main></Main>
-            <Container>
-                {/*<Login/>*/}
-            </Container>
+            <div className="contents">
+                <Container>
+                    {/*<Login/>*/}
+                    d
+                </Container>
+            </div>
         </main>
     );
 }
